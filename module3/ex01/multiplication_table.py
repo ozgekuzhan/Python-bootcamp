5bd29087ec4664adb977aa12da13f51da32c9298
Ozge Kuzhan <okuzhan@f0r6s3.codam.nl>
2025-07-23 15:55:04 +0200

    is decimal

--- module3/ex01/multiplication_table.py
+++ module3/ex01/multiplication_table.py
@@ -1,6 +1,6 @@
 num = int(input("Enter a number: "))
 print(num)
 
-for i in range(0,10):
+for i in range(0,11):
     print(num, "x", i, "=", num*i)
 
---
+++ module4/ex03/float.py
@@ -0,0 +1,7 @@
+num = input("Give me a number: ")
+res = num.isdigit()
+
+if res == True:
+    print("This number is an integer")
+else:
+    print("This number is an float ")
