3c3f1affedf406e64628e0bc3ba4b09c1952226e
Ozge Kuzhan <okuzhan@f0r6s3.codam.nl>
2025-07-23 14:02:25 +0200

    module 3 ex03 multiplication table done in for loops and while loops

---
+++ module3/ex03/advanced_mult.py
@@ -0,0 +1,13 @@
+# for i in range (0, 11):
+#     for multiplier in range (1, 11):
+#         print(i * multiplier, end=" ")
+#     print()
+
+i = 0
+while i <= 10:
+    j = 0
+    while j <= 10:
+        print(i * j, end=" ")
+        j += 1
+    print()
+    i += 1
